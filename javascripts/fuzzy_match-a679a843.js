8bca63fd02a7d2651fd648d312f313fae8386e6a
Peter Graham <peterghm@gmail.com>
2013-10-01 22:30:34 -0700

    Automated commit at 2013-10-02 05:30:34 UTC by middleman-deploy 0.1.3

--- javascripts/fuzzy_match-a679a843.js
+++
@@ -1,1 +0,0 @@
-function fuzzyMatch(h,t){var e=t.toLowerCase().split(""),i=[];return h.forEach(function(h){var t=0,o=0,r="",s=[];for(h=h.toLowerCase();o<h.length;){if(h[o]===e[t]){if(r+=highlight(h[o]),s.push(o),t++,t>=e.length){i.push({match:h,highlighted:r+h.slice(o+1),positions:s});break}}else r+=h[o];o++}}),i}
